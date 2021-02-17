e656c35c7dff51d3efe708d60f409dabb834713f
taroteeth <zarahsikora@gmail.com>
2021-02-17 16:10:08 -0500

    Button animation

--- includes/button.php
+++ includes/button.php
@@ -4,5 +4,5 @@ function button($link = null, $text = null, $class = null, $scrollTo = null) {
     if(!$link) $link = get_sub_field('link');
     if(!$text) $text = get_sub_field('text');
 
-    return '<a class="btn '. $class .'" data-scrollTo="'. $scrollTo .'" role="button" href="'. $link .'"><span>' . $text . '</span></a>';
+    return '<a class="btn '. $class .'" data-scrollTo="'. $scrollTo .'" role="button" href="'. $link .'"><div class="btn-hover-mask"></div><span>' . $text . '</span></a>';
 }
