78c60d09b42da4b269ccb2b6342daf5cc64253b7
taroteeth <zarahsikora@gmail.com>
2021-05-28 15:21:34 -0400

    Base styling for ingredients list display

--- components/ingredients-list-display.php
+++ components/ingredients-list-display.php
@@ -1,38 +1,44 @@
-<div class="module-flush ingredients-list-display">
-    <div class="row">
-        <div class="list-wrapper col-lg-6">
-            <?php if(have_rows('ingredients')): ?>
-                <ul>
-                    <?php while(have_rows('ingredients')): the_row(); ?>
-                        <?php
-                        $name = get_sub_field('name');
-                        $slug = urlencode ($name);
-                        $description = get_sub_field('description');
-                        ?>
-                        <li data-ingredient="<?php echo $slug ?>">
-                            <p><?php echo $name ?></p>
-                        </li>
-                    <?php endwhile; ?>
+<div class="module-wrapper ingredients-list-display dark-bg">
+    <div class="module-padded">
+        <div class="row">
+            <div class="list-wrapper col-lg-6">
+                <?php if(have_rows('ingredients')): ?>
+                    <ul>
+                        <?php $counter = 0 ?>
+                        <?php while(have_rows('ingredients')): the_row(); ?>
+                            <?php
+                            $name = get_sub_field('name');
+                            $slug = urlencode ($name);
+                            $description = get_sub_field('description');
+                            ?>
+                            <li data-ingredient="<?php echo $slug ?>" <?php if($counter === 0) echo 'class= "active"' ?>>
+                                <?php echo $name ?>
+                            </li>
+                            <?php $counter++ ?>
+                        <?php endwhile; ?>
                     </ul>
                 <?php endif; ?>
-        </div>
+            </div>
 
-        <div class="description-wrapper col-lg-6">
-            <?php if(have_rows('ingredients')): ?>
-                <ul>
-                    <?php while(have_rows('ingredients')): the_row(); ?>
-                        <?php
-                        $name = get_sub_field('name');
-                        $slug = urlencode ($name);
-                        $description = get_sub_field('description');
-                        ?>
-                        <li data-ingredient="<?php echo $slug ?>">
-                            <h3><?php echo $name ; ?></h3>
-                            <p><?php echo $description ?></p>
-                        </li>
-                    <?php endwhile; ?>
-                </ul>
-            <?php endif; ?>
+            <div class="description-wrapper col-lg-6">
+                <?php if(have_rows('ingredients')): ?>
+                    <ul>
+                        <?php $counter = 0 ?>
+                        <?php while(have_rows('ingredients')): the_row(); ?>
+                            <?php
+                            $name = get_sub_field('name');
+                            $slug = urlencode ($name);
+                            $description = get_sub_field('description');
+                            ?>
+                            <li data-ingredient="<?php echo $slug ?>" <?php if($counter === 0) echo 'class= "active"' ?>>
+                                <h3><?php echo $name ; ?></h3>
+                                <p><?php echo $description ?></p>
+                            </li>
+                            <?php $counter++ ?>
+                        <?php endwhile; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </div>
