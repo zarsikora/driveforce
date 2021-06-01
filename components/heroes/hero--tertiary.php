29b18e53340c4c2db72637fa4c16e8ac25de780e
taroteeth <zarahsikora@gmail.com>
2021-06-01 12:32:50 -0400

    add copy option for tertiary hero

--- components/heroes/hero--tertiary.php
+++ components/heroes/hero--tertiary.php
@@ -1,5 +1,6 @@
 <?php
     $header = get_sub_field('header');
+    $copy = get_sub_field('copy');
     $centered = get_sub_field('centered_text');
     $image = get_sub_field('image');
 ?>
@@ -11,6 +12,10 @@
             <div class="row">
                 <div class="<?php echo $centered ? ' col-md-12' : 'col-md-6'?>">
                     <h1><?php echo $header ?></h1>
+                    <?php if($copy): ?>
+                        <div class="copy"><?php echo $copy ?></div>
+                    <?php endif; ?>
+
                     <?php if($image) echo imageTag($image, '', '', '', false) ?>
                 </div>
             </div>
