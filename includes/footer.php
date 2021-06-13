508dbb9d7e566326a04efabd057fa894d1006c47
devonriley <devontriley@gmail.com>
2021-06-12 20:40:07 -0400

    fast checkout stylea

--- includes/footer.php
+++ includes/footer.php
@@ -9,8 +9,8 @@
                     <div class="upper-nav row">
                         <div class="logo col-lg-2">
                             <a class="logo-link" href="<?php echo home_url(); ?>" aria-label="Return to Home Page">
-                                <svg class="logo" viewBox="0 0 39.96 31.039">
-                                    <use href="#footer-logo"></use>
+                                <svg class="logo" viewBox="0 0 104 104">
+                                    <use href="#square-logo"></use>
                                 </svg>
                             </a>
                         </div>
