b40431c3ab3c515b32bb676fa319a68e6d66a74a
devonriley <devontriley@gmail.com>
2021-06-18 11:22:43 -0400

    tertiary hero styles

--- components/heroes/hero--tertiary.php
+++ components/heroes/hero--tertiary.php
@@ -1,23 +1,23 @@
 <?php
     $header = get_sub_field('header');
     $copy = get_sub_field('copy');
-    $centered = get_sub_field('centered_text');
     $image = get_sub_field('image');
 ?>
 
 <div class="jumbotron hero tertiary module-flush">
-
     <div class="inner">
-        <div class="text-container <?php if($centered) echo ' align-center'?>">
+        <div class="text-container">
             <div class="row">
-                <div class="<?php echo $centered ? ' col-md-12' : 'col-md-6'?>">
-                    <h1><?php echo $header ?></h1>
+
+                <div class="col-md-8 offset-md-2">
+                    <h1 class="text-center"><?php echo $header ?></h1>
                     <?php if($copy): ?>
                         <div class="copy"><?php echo $copy ?></div>
                     <?php endif; ?>
 
                     <?php if($image) echo imageTag($image, '', '', '', false) ?>
                 </div>
+
             </div>
         </div>
     </div>
