f7e3bab20f305b7a7bd064763fa5c749caefd14d
devonriley <devontriley@gmail.com>
2021-06-18 11:28:34 -0400

    large text module styling

--- components/large-text-module.php
+++ components/large-text-module.php
@@ -2,12 +2,15 @@
 $text = get_sub_field('text');
 ?>
 
-
 <div class="module-wrapper large-text-module dark-bg">
     <div class="module-padded">
-        <div class="text-container">
-            <div class="copy" data-animation-effect="moduleFadeIn" data-animation-trigger="scroll">
-                <?php echo $text ?>
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <div class="text-container">
+                    <div class="copy" data-animation-effect="moduleFadeIn" data-animation-trigger="scroll">
+                        <?php echo $text ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
