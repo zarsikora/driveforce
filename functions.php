edd5151c0fda3c12034f9aad7ca7ceed696aac71
devonriley <devontriley@gmail.com>
2021-06-21 08:52:07 -0400

    remove zoom from pdp gallery

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ function bones_theme_setup() {
     );
 
     add_theme_support( 'woocommerce' );
-    add_theme_support( 'wc-product-gallery-zoom' );
+    //add_theme_support( 'wc-product-gallery-zoom' );
     add_theme_support( 'wc-product-gallery-lightbox' );
     add_theme_support( 'wc-product-gallery-slider' );
 }
