06cb5e44343a7cd808e8c6f9f9a5a37068ac7b17
devonriley <devontriley@gmail.com>
2021-06-23 08:45:08 -0400

    add coupon form to checkout

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -36,6 +36,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
 		<div class="col2-set" id="customer_details">
+
 			<div class="col-1">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 			</div>
