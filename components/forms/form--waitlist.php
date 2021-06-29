d5362e4613412f658a4d3a65e910a69235e170b5
devonriley <devontriley@gmail.com>
2021-06-29 16:02:54 -0400

    disable footer button until filled out and add required asterisks

--- components/forms/form--waitlist.php
+++ components/forms/form--waitlist.php
@@ -2,15 +2,15 @@
     <form autocomplete="off" action="" method="post" id="sharpspring-waitlist" class="sharpspring-waitlist" novalidate>
         <div class="field-group">
             <label class="sr-only">First Name</label>
-            <input class="form-control" type="text" name="first-name" value="" placeholder="First Name" />
+            <input class="form-control" type="text" name="first-name" value="" placeholder="First Name*" />
         </div>
         <div class="field-group">
             <label class="sr-only">Last Name</label>
-            <input class="form-control" type="text" name="last-name" value="" placeholder="Last Name" />
+            <input class="form-control" type="text" name="last-name" value="" placeholder="Last Name*" />
         </div>
         <div class="field-group last-field">
             <label class="sr-only">Email</label>
-            <input class="form-control" type="text" name="email" value="" placeholder="Email" />
+            <input class="form-control" type="text" name="email" value="" placeholder="Email*" />
         </div>
         <button disabled class="btn small" type="submit">
             <div class="btn-hover-mask"></div>
